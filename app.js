66568063e720b6d07ad797fe953dce7e737f7098
wuchunyu <45424620+wuchunyu@users.noreply.github.com>
2023-06-21 20:42:59 +0800

    1

---
+++ app.js
@@ -0,0 +1,28 @@
+import updateManager from './common/updateManager';
+
+App({
+  globalData: {
+		searchValue: '',
+	},
+  onLaunch: function () {},
+  onShow: function () {
+    updateManager();
+  },
+  // 监听全局变量变化
+  watch: function (variate, method) {
+    var obj = this.globalData;
+    let val = obj[variate];// 单独变量来存储原来的值
+    Object.defineProperty(obj, variate, {
+      configurable: false,
+      enumerable: true,
+      set: function (value) {
+        val = value;// 重新赋值
+        method(variate,value);// 执行回调方法
+      },
+      get: function () {
+        // 在其他界面调用getApp().globalData.variate的时候，这里就会执行。
+        return val; // 返回当前值
+      }
+    })
+  }
+});
